62584acb030594982683415d0421ac389d066fe9
andreiribac <ribacandrei@gmail.com>
2021-11-19 16:15:31 +0200

    lesson 5

--- src/App.js
+++ src/App.js
@@ -1,17 +1,52 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Routes, Route } from "react-router-dom";
+import axios from 'axios';
 import './App.css';
 import { Header } from './components';
 import { Home, Cart } from './pages';
 
-
+const categoriesArr = [
+	"Мясные",
+	"Вегетарианская",
+	"Гриль",
+	"Острые",
+	"Закрытые",
+];
+const sortsArr = [
+	"популярности",
+	"цене",
+	"алфавиту",
+];
 function App() {
+
+	const [pizzas, setPizzas] = React.useState([])
+
+	useEffect(() => {
+		axios.get('http://localhost:3000/db.json').then(({ data }) => {
+			setPizzas(data.pizzas)
+		});
+		// fetch('http://localhost:3000/db.json')
+		// 	.then((resp) => {
+		// 		console.log('fetch', resp);
+		// 		return resp.json()
+		// 	})
+		// 	.then(json => {
+		// 		setPizzas(json.pizzas)
+		// 	});
+	}, [])
+
 	return (
 		<div className="wrapper">
 			<Header />
 			<div className="content">
 				<Routes>
-					<Route exact path="/" element={<Home />} />
+					<Route exact path="/" element={
+						<Home
+							category={categoriesArr}
+							sort={sortsArr}
+							pizzas={pizzas}
+						/>
+					} />
 					<Route exact path="cart" element={<Cart />} />
 					<Route path="*" element={"ошибка 404"} />
 				</Routes>
--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './scss/app.scss';
 import App from './App';
-import { BrowserRouter, Routes, Route, } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 // import reportWebVitals from './reportWebVitals';
-import Header from './components/Header';
 
 ReactDOM.render(
   <React.StrictMode>
